cf7b192905089c39d8d00e40c1c361b8662b8008
SymTr <sayumi.tsuruno@gmail.com>
2024-05-04 15:06:26 +0900

    編集機能を実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -21,12 +21,20 @@ class PrototypesController < ApplicationController
   end
   
   def edit
+    @prototype = Prototype.find(params[:id])
   end
 
   def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+     redirect_to prototype_path
+    else 
+      render :edit, status: :unprocessable_entity
+    end
   end
   
   private
+  
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
